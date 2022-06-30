5dce35747b623c2b9c8520fb56491f11c3d85b30
Scott <scott@linfords.com>
2022-06-29 18:52:41 -0700

    Problem 007 passes

--- Problem-007.py
+++ Problem-007.py
@@ -12,8 +12,8 @@ the_answer = 104743
 
 
 def is_prime(n):
-    for i in range(2, n):
-        if (n % i) == 0:
+    for j in range(2, n):
+        if (n % j) == 0:
             return False
     return True
 
