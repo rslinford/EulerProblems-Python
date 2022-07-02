e02d7df97b3c9a5e85e9ce56224884569cfd6750
Scott <scott@linfords.com>
2022-07-02 08:40:48 -0700

    Problem 024 passes

--- Problem-024.py
+++ Problem-024.py
@@ -1,3 +1,4 @@
+import itertools
 import time
 from math import factorial
 
@@ -13,12 +14,21 @@ lexicographic permutations of 0, 1 and 2 are:
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 """
 
-answer = 0
-the_answer = 2783915460
+answer = ""
+the_answer = "2783915460"
 start_time = time.time()
 
+permutation_list = list(itertools.permutations(range(0, 10)))
+print("Millionth lexicographic permutation + " + str(permutation_list[999999]))
+
+millionth = permutation_list[999999]
+s = ""
+for x in millionth:
+    s += str(x)
+answer = s
+
 elapsed_time = time.time() - start_time
-print("Answer %d in %d seconds" % (answer, elapsed_time))
+print("Answer %s in %d seconds" % (answer, elapsed_time))
 if answer == the_answer:
     print("Pass")
 else:
